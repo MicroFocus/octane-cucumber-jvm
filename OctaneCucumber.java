cd0a0bfa8a6290a134c5f4f394459dd15b312a7d
intract <michael.intract@hp.com>
2016-06-21 10:43:05 +0300

    new approach - fix

--- OctaneCucumber.java
+++ OctaneCucumber.java
@@ -1,45 +1,18 @@
-import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import cucumber.runtime.Runtime;
+import cucumber.runtime.RuntimeOptions;
+import cucumber.runtime.io.ResourceLoader;
 import org.junit.runners.model.InitializationError;
 
 import java.io.IOException;
-import java.lang.reflect.Field;
-import java.lang.reflect.Proxy;
-import java.util.*;
 
 public class OctaneCucumber extends Cucumber {
-    private String OCTANE_FORMATTER = "HPEAlmOctaneGherkinFormatter";
-
-    private static final ThreadLocal<String[]> instance = new ThreadLocal<>();
-
-    public static String[] getFeatures() {
-        return instance.get();
-    }
-
     public OctaneCucumber(Class clazz) throws IOException, InitializationError {
         super(clazz);
-        CucumberOptions cucumberOptions = (CucumberOptions) clazz.getAnnotation(CucumberOptions.class);
-        instance.set(cucumberOptions.features());
-        addFormatterToOptions(cucumberOptions);
     }
 
-    private void addFormatterToOptions(CucumberOptions cucumberOptions) {
-        try {
-            Object handler = Proxy.getInvocationHandler(cucumberOptions);
-            Field memberValuesField = handler.getClass().getDeclaredField("memberValues");
-            memberValuesField.setAccessible(true);
-            Map<String, Object> memberValues = (Map<String, Object>) memberValuesField.get(handler);
-            String[] originalPlugins = (String[]) memberValues.get("plugin");
-
-            List<String> newPlugins = originalPlugins == null ? new ArrayList<>() : new ArrayList(Arrays.asList(originalPlugins));
-            if (newPlugins.contains(OCTANE_FORMATTER)) {
-                return;
-            }
-            newPlugins.add(OCTANE_FORMATTER);
-            String[] result = new String[newPlugins.size()];
-            memberValues.put("plugin", newPlugins.toArray(result));
-        } catch (Exception e) {
-            System.out.println("Failed to add formatter to plugin: " + e);
-        }
+    protected Runtime createRuntime(ResourceLoader resourceLoader, ClassLoader classLoader, RuntimeOptions runtimeOptions) throws InitializationError, IOException {
+        runtimeOptions.addPlugin(new HPEAlmOctaneGherkinFormatter(runtimeOptions.getFeaturePaths()));
+        return super.createRuntime(resourceLoader, classLoader, runtimeOptions);
     }
 }
