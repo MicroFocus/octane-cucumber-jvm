f060b0210cdd9146dfcf0b8d6d169100ceae9393
intract <michael.intract@hp.com>
2016-09-27 15:07:04 +0300

    tech: fix tests

--- src/test/java/com/hpe/alm/octane/RunCucumberFeatureClassPathTest.java
+++ src/test/java/com/hpe/alm/octane/RunCucumberFeatureClassPathTest.java
@@ -5,7 +5,7 @@ import cucumber.api.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(OctaneCucumber.class)
-@CucumberOptions(plugin = {"junit:cuc_report.xml"},features = {"classpath:F1\\test1.feature","classpath:F2\\test2.feature"})
+@CucumberOptions(plugin = {"junit:cuc_report.xml"},features = {"classpath:com\\hpe\\alm\\octane\\F1\\test1.feature","classpath:com\\hpe\\alm\\octane\\F2\\test2.feature"})
 public class RunCucumberFeatureClassPathTest {
 
 }
