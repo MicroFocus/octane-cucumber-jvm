d45f1cd02733d0af8b489b2eaba5b7ea547b9ad7
franksha <sharon.frank@microfocus.com>
2019-05-02 10:45:30 +0300

    Handle wrong flows + code readablity

--- src/main/java/com/hpe/alm/octane/infra/TestTracker.java
+++ src/main/java/com/hpe/alm/octane/infra/TestTracker.java
@@ -1,5 +1,7 @@
 package com.hpe.alm.octane.infra;
 
+import cucumber.runtime.CucumberException;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -20,24 +22,37 @@ public class TestTracker {
     }
 
     public void setCurrentScenario(String scenarioName) {
-        if (currentScenario != null && currentScenario.getName().equals(scenarioName)) {
-            if (currentScenario.getOutlineIndex() == 0) {
-               currentScenario.setOutlineIndex(1);
-               outlineIndex = 1;
+        if (isOutlineScenario(scenarioName)) {
+            if (isFirstOutlineScenario(currentScenario)) {
+                currentScenario.setOutlineIndex(++outlineIndex);
             }
             currentScenario = new ScenarioElement(scenarioName, ++outlineIndex);
         } else {
-            currentScenario = new ScenarioElement(scenarioName, 0);
             outlineIndex = 0;
+            currentScenario = new ScenarioElement(scenarioName, outlineIndex);
         }
         currentFeature.addScenario(currentScenario);
     }
 
+    private boolean isOutlineScenario(String scenarioName) {
+        return currentScenario != null && currentScenario.getName().equals(scenarioName);
+    }
+
+    private boolean isFirstOutlineScenario(ScenarioElement scenario) {
+        return scenario.getOutlineIndex() == 0;
+    }
+
     public void setCurrentStep(StepElement step) {
+        if (currentScenario == null) {
+            throw new CucumberException("Flow error - Tried to set step when there is no current scenario");
+        }
         currentScenario.addStep(step);
     }
 
     public StepElement getCurrentStep() {
+        if (currentScenario == null) {
+            return null;
+        }
         return currentScenario.getSteps().get(currentScenario.getSteps().size()-1);
     }
 
