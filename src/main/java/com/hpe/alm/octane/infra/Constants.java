a57ee39d848caf51141dddd1fd138b0fc0cc870d
franksha <sharon.frank@microfocus.com>
2019-05-02 10:46:21 +0300

    Add space to error constant

--- src/main/java/com/hpe/alm/octane/infra/Constants.java
+++ src/main/java/com/hpe/alm/octane/infra/Constants.java
@@ -2,7 +2,7 @@ package com.hpe.alm.octane.infra;
 
 public class Constants {
 	public static final String RESULTS_FOLDER = "gherkin-results";
-	public static final String errorPrefix = "<OctaneGherkinFormatter Error>";
+	public static final String errorPrefix = "<OctaneGherkinFormatter Error> ";
 	public static final String XML_VERSION = "1";
 	public static final String TAG_ID = "@TID";
 }
